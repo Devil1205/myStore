1b23a632805dfe8a33f55cd1267242d186a074ed
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2024-03-18 00:55:39 +0530

    Deployed backend to cyclic.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://clear-red-handbag.cyclic.app/"],
     credentials: true
 }));
 
